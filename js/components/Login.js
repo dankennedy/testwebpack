7c5b6a5096d8bc5c4c5a36e8a887d02cd78f09f5
dankennedy <daniel@firstcs.co.uk>
2015-06-17 09:46:58 +0100

    running react app on webpack dev server

---
+++ js/components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+let Login = React.createClass({
+
+  render() {
+    return(<div>Welcome to the login page (updated) and again</div>);
+  }
+});
+
+export default Login;
