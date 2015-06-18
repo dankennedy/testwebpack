e826400a6a8799b45dc01dcc0405e2ccd9b4f9d7
dankennedy <daniel@firstcs.co.uk>
2015-06-18 20:54:53 +0100

    start of booking form

--- lib/client/pages/Book.jsx
+++ lib/client/pages/Book.jsx
@@ -1,13 +1,19 @@
 'use strict';
 
 import React from 'react';
+import FormField from '../components/FormField';
 
 let Book = React.createClass({
 
   render() {
     return(
       <div className="Book">
-        <h2>Book</h2>
+        <FormField id="arrivaldate" label="Arrival Date" type="date" />
+        <FormField id="numberofnights" label="Number of nights" type="number" />
+        <FormField id="firstname" label="First name" />
+        <FormField id="lastname" label="Last name" />
+        <FormField id="email" label="Email" type="email" />
+        <FormField id="address" label="Address" />
       </div>
     );
   }
