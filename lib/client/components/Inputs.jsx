512677f04f0c315633d23851e41f2515cdb2a9d8
dankennedy <daniel@firstcs.co.uk>
2015-06-23 15:14:38 +0100

    mixin for validation on inputs

--- lib/client/components/Inputs.jsx
+++ lib/client/components/Inputs.jsx
@@ -4,13 +4,17 @@ import React from 'react';
 import {classNames} from '../../shared/utils';
 import Validations from '../../shared/validations';
 
-export var Input = React.createClass({
-
-  getInitialProps() {
+var InputMixin = {
+  getDefaultProps() {
     return {validations: []};
   },
   getInitialState() {
-    return {value: this.props.value, isvalid: true, validationmessage: ""};
+    return {
+      value: this.props.value,
+      isValid: true,
+      validationMessage: "",
+      validationsAdded: false
+    };
   },
   handleChange(event) {
     var val = event.target.value;
@@ -20,33 +24,46 @@ export var Input = React.createClass({
   validate(val) {
     let vals = this.props.validations || [];
 
-    switch (this.props.type) {
-      case "email" :
-        vals.push(new Validations.EmailAddress());
-        break;
-
+    if (!this.state.validationsAdded) {
+        switch (this.props.type) {
+          case "email" :
+            vals.push(new Validations.EmailAddress());
+            break;
+          case "number" :
+            vals.push(new Validations.IsNumber());
+            break;
+        }
+        this.setState({validationsAdded: true});
     }
 
     if (vals.length) {
       for (var i = 0; i < vals.length; i++) {
         if (!vals[i].validate(val)) {
-          this.setState({isvalid: false, validationmessage: vals[i].message});
+          this.setState({isValid: false, validationMessage: vals[i].validationMessage});
           return;
         }
       }
-      this.setState({isvalid: true, validationmessage: ""});
+      this.setState({isValid: true, validationMessage: ""});
     }
+  },
+};
+
+export var Input = React.createClass({
+  mixins: [InputMixin],
+  getDefaultProps() {
+    return {type: "text"};
   },
   render() {
     return(
-      <div className={classNames("formfield", {"invalid": !this.state.isvalid})}>
+      <div className={classNames("formfield", {"invalid": !this.state.isValid})}>
         <label htmlFor={this.props.id}>{this.props.label}</label>
         <input type={this.props.type || "text"}
                id={this.props.id}
                name={this.props.id}
                defaultValue={this.state.value}
                placeholder={this.props.placeholder}
-               maxlength={this.props.maxlength}
+               maxLength={this.props.maxlength}
+               title={this.state.validationMessage}
                onChange={this.handleChange}>
         </input>
       </div>
@@ -56,10 +73,13 @@ export var Input = React.createClass({
 
 
 export var NumberInput = React.createClass({
-
+  mixins: [InputMixin],
+  getDefaultProps() {
+    return {type: "number"};
+  },
   render() {
     return(
-      <div className="formfield">
+      <div className={classNames("formfield", {"invalid": !this.state.isValid})}>
         <label htmlFor="">{this.props.label}</label>
         <input type="number"
                id={this.props.id}
@@ -67,7 +87,9 @@ export var NumberInput = React.createClass({
                min={this.props.min}
                max={this.props.max}
                placeholder={this.props.placeholder}
-               defaultValue={this.props.value}>
+               defaultValue={this.props.value}
+               title={this.state.validationMessage}
+               onChange={this.handleChange}>
         </input>
       </div>
     );
@@ -75,16 +97,18 @@ export var NumberInput = React.createClass({
 });
 
 export var TextArea = React.createClass({
-
+  mixins: [InputMixin],
   render() {
     return(
-      <div className="formfield">
+      <div className={classNames("formfield", {"invalid": !this.state.isValid})}>
         <label htmlFor="">{this.props.label}</label>
         <textarea
                id={this.props.id}
                name={this.props.id}
                placeholder={this.props.placeholder}
-               defaultValue={this.props.value}>
+               defaultValue={this.props.value}
+               title={this.state.validationMessage}
+               onChange={this.handleChange}>
         </textarea>
       </div>
     );
--- lib/client/pages/Book.jsx
+++ lib/client/pages/Book.jsx
@@ -7,35 +7,59 @@ import {Input, NumberInput} from '../components/Inputs';
 let Book = React.createClass({
 
   handleClick() {
-    console.log(this.refs.arrivaldate);
-    console.log(this.refs.arrivaldate.state.value);
+    console.log({
+      'Arrival Date': this.refs.arrivaldate.state.value,
+      'Nights': this.refs.numberofnights.state.value,
+      'Firstname': this.refs.firstname.state.value,
+      'Lastname': this.refs.lastname.state.value,
+      'Email': this.refs.email.state.value,
+      'Address': this.refs.address.state.value}
+      );
   },
   render() {
     return(
       <div className="Book">
 
-        <Input id="arrivaldate" label="Arrival Date" type="date" ref="arrivaldate"
+        <Input id="arrivaldate"
+          label="Arrival Date"
+          type="date"
+          ref="arrivaldate"
           placeholder="Required (dd/mm/yyyy)"
           validations={[new validations.Required()]} />
 
-        <NumberInput id="numberofnights" label="Number of nights" value={2} min={2} max={14}
-          validations={[new validations.Required()]} />
+        <NumberInput id="numberofnights"
+          label="Number of nights"
+          ref="numberofnights"
+          value={2}
+          min={2}
+          max={14}
+          validations={[new validations.Required(), new validations.WithinRange(2, 14)]} />
 
-        <Input id="firstname" label="First name"
+        <Input id="firstname"
+          label="First name"
+          ref="firstname"
           placeholder="Required"
           maxlength={100}
           validations={[new validations.Required(), new validations.StringLength(100)]} />
 
-        <Input id="lastname" label="Last name"
+        <Input id="lastname"
+          label="Last name"
+          ref="lastname"
           placeholder="Required"
           maxlength={100}
           validations={[new validations.Required(), new validations.StringLength(100)]} />
 
-        <Input id="email" label="Email" type="email"
+        <Input id="email"
+          label="Email"
+          ref="email"
+          type="email"
           placeholder="Required"
           validations={[new validations.Required()]} />
 
-        <Input id="address" label="Address" />
+        <Input id="address"
+          label="Address"
+          ref="address"
+          validations={[new validations.StringLength(255)]} />
 
         <button type="submit" onClick={this.handleClick}>Submit</button>
 
