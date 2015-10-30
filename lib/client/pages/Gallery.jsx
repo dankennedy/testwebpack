a2afe1275e9f17baf801a4743c8e960dad6e13b5
dankennedy <daniel@firstcs.co.uk>
2015-10-30 13:07:39 +0000

    working gallery

--- lib/client/pages/Gallery.jsx
+++ lib/client/pages/Gallery.jsx
@@ -1,16 +1,34 @@
 'use strict';
 
 import React from 'react';
+import axios from 'axios';
+
+import ImageGallery from '../components/ImageGallery';
 
 let Gallery = React.createClass({
 
-  render() {
-    return(
-      <div className="Gallery">
-        <h2>Gallery</h2>
-      </div>
-    );
-  }
+    getInitialState() {
+        return {images: []};
+    },
+
+    componentDidMount() {
+        axios.get('/api/gallery').then(function(response) {
+            this.setState({
+                images: response.data
+            });
+        }.bind(this)).catch(function(response) {
+            console.error(response);
+        });
+    },
+
+    render() {
+          return (
+            <ImageGallery
+              items={this.state.images}
+              autoPlay={true}
+              slideInterval={4000}/>
+          );
+      }
 });
 
 export default Gallery;
