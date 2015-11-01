d4e984d01a1e39eb5f99a020d60eb2d83f6be81c
dankennedy <daniel@firstcs.co.uk>
2015-11-01 12:25:29 +0000

    tests for booking

--- test/bookings.js
+++ test/bookings.js
@@ -62,6 +62,76 @@ suite('Booking Utils', function() {
                 );
         });
 
+        test('should default created date to now', function() {
+            assert(moment().isSame(new Booking().getCreatedDate(), 'minute'));
+        });
+
+        test('should default deposit due date to now', function() {
+            assert(moment().isSame(new Booking().getDepositDueDate(), 'minute'));
+        });
+
+        test('should calculate balance due date for 6 weeks before arrival date', function() {
+            let balanceDueDate = new Booking({
+                    createdDate: new Date(2015, 1, 1),
+                    arrivalDate: new Date(2015, 9, 26),
+                }).getBalanceDueDate(),
+                expected = moment(new Date(2015, 8, 14));
+
+            assert(expected.isSame(balanceDueDate, 'day'),
+                `expected ${expected.format('YYYY-MM-DD')} but got ${balanceDueDate.format('YYYY-MM-DD')}`);
+        });
+
+        test('getDepositPayment uses type to filter', function() {
+            assert.deepEqual(getBookingWithPayments().getDepositPayment(), {type: 'deposit', amount: 20});
+        });
+
+        test('getBalancePayment uses type to filter', function() {
+            assert.deepEqual(getBookingWithPayments().getBalancePayment(), {type: 'balance', amount: 10});
+        });
+
+        test('getTotalPayments sums amounts', function() {
+            assert.equal(getBookingWithPayments().getTotalPayments(), 30);
+        });
+
+        test('getBalanceOutstanding substracts payments from price', function() {
+            assert.equal(getBookingWithPayments(50).getBalanceOutstanding(), 20);
+        });
+
+        test('isDepositPaid checks for deposit exists', function() {
+            assert(getBookingWithPayments().isDepositPaid());
+        });
+
+        test('isDepositPaid checks for deposit', function() {
+            assert(!new Booking().isDepositPaid());
+        });
+
+        test('isBalancePaid checks for balance exists', function() {
+            assert(getBookingWithPayments().isBalancePaid());
+        });
+
+        test('isBalancePaid checks for balance', function() {
+            assert(!new Booking().isBalancePaid());
+        });
+
+        test('isFullyPaid checks for both payments exists', function() {
+            assert(getBookingWithPayments().isFullyPaid());
+        });
+
+        test('isFullyPaid checks for both payments', function() {
+            assert(!new Booking({price: 10}).isFullyPaid());
+        });
+
+        test('hasUsedVoucher checks for voucher exists', function() {
+            assert(new Booking({voucher: {_id: 'test'}}).hasUsedVoucher());
+        });
+
+        test('hasUsedVoucher checks for voucher', function() {
+            assert(!new Booking().hasUsedVoucher());
+        });
+
+        test('getBalance checks for voucher', function() {
+            assert.equal(getBookingWithPayments(50, 20).getBalance(), 30);
+        });
     });
 
     function getExistingBookings() {
@@ -72,4 +142,10 @@ suite('Booking Utils', function() {
         return bookings;
     }
 
+    function getBookingWithPayments(price, deposit) {
+        return new Booking({price: price, deposit: deposit, payments: [
+                {type: 'balance', amount: 10},
+                {type: 'deposit', amount: 20}
+                ]});
+    }
 });
