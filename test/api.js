1dc2fdbb00e7d706bd928b9a72e06a33bbee863c
dankennedy <daniel@firstcs.co.uk>
2015-11-10 11:00:38 +0000

    MOAR api tests

--- test/api.js
+++ test/api.js
@@ -27,25 +27,34 @@ suite('API', function() {
                 .get('/api/bookings/52d937dcacff62664a000024')
                 .expect('Content-Type', /json/)
                 .expect(200, {
-                    "_id" : "52d937dcacff62664a000024",
-                    "deposit" : 49,
-                    "arrivalDate" : "2014-04-24T00:00:00.000Z",
-                    "numberOfNights" : 3,
-                    "title" : "Mrs",
-                    "firstname" : "Bea",
-                    "lastname" : "Firth",
-                    "email" : "morganaclothing@aol.com",
-                    "phone" : "",
-                    "price" : 195,
-                    "payments" : [],
-                    "address" : [
-                        "Not provided"
+                    '_id' : '52d937dcacff62664a000024',
+                    'deposit' : 49,
+                    'arrivalDate' : '2014-04-24T00:00:00.000Z',
+                    'numberOfNights' : 3,
+                    'title' : 'Mrs',
+                    'firstname' : 'Bea',
+                    'lastname' : 'Firth',
+                    'email' : 'morganaclothing@aol.com',
+                    'phone' : '',
+                    'price' : 195,
+                    'payments' : [],
+                    'address' : [
+                        'Not provided'
                     ],
-                    "createdDate" : "2014-01-17T14:02:04.690Z",
-                    "__v" : 0
+                    'createdDate' : '2014-01-17T14:02:04.690Z',
+                    '__v' : 0
                 }, done);
         });
 
+        test('GET price for booking', function(done) {
+            request(app)
+                .get('/api/bookingprice?arrivalDate=2014-04-05&numberOfNights=3')
+                .expect('Content-Type', /json/)
+                .expect('Cache-Control', /public/)
+                .expect(200, {
+                    'price' : 195
+                }, done);
+        });
     });
 });
 
