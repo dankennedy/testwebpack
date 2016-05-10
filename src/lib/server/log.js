7d34af338258d887b6d53f496fba2774693a8ffa
dankennedy <daniel@firstcs.co.uk>
2016-05-10 21:24:35 +0100

    final logging tweaks

--- src/lib/server/log.js
+++ src/lib/server/log.js
@@ -1,57 +1,50 @@
 'use strict';
 
-var bunyan         = require('bunyan'),
-    config         = require('server/config').logging,
-    PrettyStream   = require('bunyan-prettystream'),
+var bunyan = require('bunyan'),
+    config = require('server/config').logging,
+    PrettyStream = require('bunyan-prettystream'),
     LogstashStream = require('bunyan-logstash-tcp');
 
-
-var stdout = new PrettyStream({mode:'dev'});
+var stdout = new PrettyStream({ mode: 'dev' });
 stdout.pipe(process.stdout);
 
-let streams = [{ level: config.stdout || config.level, type:'raw', stream: stdout }];
-if (config.file) {
-  streams.push({ level: config.file, type:'file', path: config.default });
+let streams = [{ level: config.stdout || config.level, type: 'raw', stream: stdout }];
+
+if (config.logstash) {
+    let outStream = LogstashStream.createStream({
+        host: config.logstash.host,
+        port: config.logstash.port,
+        ssl_enable: true,
+        ca: [config.logstash.ca_file],
+        name: 'tcwhitby',
+        tags: config.logstash.tags,
+    });
+
+    outStream.on('connect', function() {
+    });
+    outStream.on('error', function(e) {
+        console.log('ERROR: ', e);
+    });
+    outStream.on('timeout', function() {
+        console.log('WARN : Logstash stream timed out');
+    });
+
+    streams.push({ type: 'raw', stream: outStream });
 }
 
-var outStream = LogstashStream.createStream({
-        host: 'logs.firstcs.co.uk',
-        port: 9506,
-        ssl_enable: true,
-        ca: ['/Users/dan/logstash-forwarder.crt'],
-        name: 'tcwhitby'
-      });
-
-outStream.on('connect', function() {
-    console.log('Connected!');
-});
-outStream.on('error', function(e) {
-    console.log('ERROR: ', e);
-});
-outStream.on('timeout', function() {
-    console.log('WARN : Timed out');
-});
-
-streams.push({type: 'raw',stream: outStream});
-
 var logger = bunyan.createLogger({
-  name:'logger',
-  level: config.level,
-  streams: streams
-});
-
-process.on('SIGUSR2', function () {
-  logger.reopenFileStreams();
+    name: 'logger',
+    level: config.level,
+    streams: streams
 });
 
 module.exports = logger;
 
 logger.errorLog = function() {
-  return require('express-bunyan-logger').errorLogger({
-    name: 'error',
-    streams: [
-      {type: 'raw', stream: stdout},
-      {path: config.error || 'log/error.log' }
-    ]
-  });
+    return require('express-bunyan-logger').errorLogger({
+        name: 'error',
+        streams: [
+            { type: 'raw', stream: stdout }
+        ]
+    });
 };
